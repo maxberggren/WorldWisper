ae61fe6580d556585b884fb6a9f9f4d6f3e31dc8
Max Berggren <maxberggren@gmail.com>
2014-10-09 20:41:55 +0200

    now i can graph the language pairs, need to find out how to travel to them all

--- walla.py
+++ walla.py
@@ -4,6 +4,9 @@
 from config import *
 from gengo import Gengo
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+from sets import Set
 
 def pretty(fromgengo):
     return json.dumps(fromgengo,
@@ -49,4 +52,31 @@ data = {
 #print pretty(gengo.getTranslationJobs(status="approved", count=15))
 #print pretty(gengo.getTranslationJob(id=1046572, pre_mt=0))
 #print pretty(gengo.getServiceLanguagePairs(lc_src='de'))
-print pretty(gengo.getServiceLanguagePairs())
+#print pretty(gengo.getServiceLanguagePairs()['response'])
+
+
+G = nx.Graph()
+languages = Set()
+
+for row in gengo.getServiceLanguagePairs()['response']:
+    # Wierdly enough we start by adding the edges
+    G.add_edge(row['lc_src'], row['lc_tgt'])
+
+# Make all languages nodes
+for language in languages:
+    G.add_node(language)
+
+#pos = nx.spring_layout(G)
+pos = nx.shell_layout(G)
+
+nx.draw_networkx_edges(G, pos, width=2.0, alpha=0.5)
+nx.draw_networkx_nodes(G, pos, node_color='w', node_size=500, alpha=1)
+
+labels={}
+for item in nx.nodes(G):
+    labels[item] = item
+
+nx.draw_networkx_labels(G, pos, labels,font_size=10)
+
+plt.axis('off')
+plt.show()
